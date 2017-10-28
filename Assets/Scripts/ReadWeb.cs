5122867dad788e6d5198de0be621fbd9dc204eed
Tim Graupmann <tgraupmann@gmail.com>
2017-10-28 10:02:20 -0700

    Connected inputs

--- Assets/Scripts/ReadWeb.cs
+++ Assets/Scripts/ReadWeb.cs
@@ -6,9 +6,15 @@ public class ReadWeb : MonoBehaviour
 {
 
     public string _mIPAddress = string.Empty;
+    public GameObject _mThumb = null;
+    public GameObject _mIndex = null;
+    public GameObject _mMiddle = null;
+    public GameObject _mRing = null;
+    public GameObject _mPinky = null;
     private FingerData _mFingerData = null;
     private FingerData _mStraight = null;
     private FingerData _mFist = null;
+    private Dictionary<GameObject, Vector3> _mOriginalEulers = new Dictionary<GameObject, Vector3>();
 
     [System.Serializable]
     public class FingerData
@@ -22,10 +28,18 @@ public class ReadWeb : MonoBehaviour
 
     IEnumerator Start()
     {
+        // save original eulers
+        SetOriginalEulers(_mThumb);
+        SetOriginalEulers(_mIndex);
+        SetOriginalEulers(_mMiddle);
+        SetOriginalEulers(_mRing);
+        SetOriginalEulers(_mPinky);
+
         if (string.IsNullOrEmpty(_mIPAddress))
         {
             yield break;
         }
+
         while (true)
         {
             string url = string.Format("http://{0}", _mIPAddress);
@@ -35,7 +49,7 @@ public class ReadWeb : MonoBehaviour
             www.Dispose();
             //Debug.Log(json);
             _mFingerData = JsonUtility.FromJson<FingerData>(json);
-            yield return null;
+            yield return new WaitForSeconds(0.1f);
         }
     }
 
@@ -86,5 +100,124 @@ public class ReadWeb : MonoBehaviour
 
         GUILayout.FlexibleSpace();
         GUILayout.EndVertical();
+    }
+
+    void SetOriginalEulers(GameObject go)
+    {
+        if (go)
+        {
+            _mOriginalEulers[go] = go.transform.localRotation.eulerAngles;
+        }
+    }
+
+    Vector3 GetOriginalEulers(GameObject go)
+    {
+        if (go &&
+            _mOriginalEulers.ContainsKey(go))
+        {
+            return _mOriginalEulers[go];
+        }
+        else
+        {
+            return Vector3.zero;
+        }
+    }
+
+    float GetInverseLerpThumb()
+    {
+        if (null == _mStraight ||
+            null == _mFist ||
+            null == _mFingerData)
+        {
+            return 0f;
+        }
+        return Mathf.InverseLerp(_mStraight.thumb, _mFist.thumb, _mFingerData.thumb);
+    }
+
+    float GetInverseLerpIndex()
+    {
+        if (null == _mStraight ||
+            null == _mFist ||
+            null == _mFingerData)
+        {
+            return 0f;
+        }
+        return Mathf.InverseLerp(_mStraight.index, _mFist.index, _mFingerData.index);
+    }
+
+    float GetInverseLerpMiddle()
+    {
+        if (null == _mStraight ||
+            null == _mFist ||
+            null == _mFingerData)
+        {
+            return 0f;
+        }
+        return Mathf.InverseLerp(_mStraight.middle, _mFist.middle, _mFingerData.middle);
+    }
+
+    float GetInverseLerpRing()
+    {
+        if (null == _mStraight ||
+            null == _mFist ||
+            null == _mFingerData)
+        {
+            return 0f;
+        }
+        return Mathf.InverseLerp(_mStraight.ring, _mFist.ring, _mFingerData.ring);
+    }
+
+    float GetInverseLerpPinky()
+    {
+        if (null == _mStraight ||
+            null == _mFist ||
+            null == _mFingerData)
+        {
+            return 0f;
+        }
+        return Mathf.InverseLerp(_mStraight.pinky, _mFist.pinky, _mFingerData.pinky);
+    }
+
+    private void Update()
+    {
+        if (_mThumb)
+        {
+            Vector3 eulers = GetOriginalEulers(_mThumb);
+            float t = GetInverseLerpThumb();
+            eulers.z -= Mathf.Lerp(0, 90, t);
+            _mThumb.transform.localRotation = Quaternion.Euler(eulers);
+        }
+
+        if (_mIndex)
+        {
+            Vector3 eulers = GetOriginalEulers(_mIndex);
+            float t = GetInverseLerpIndex();
+            eulers.z -= Mathf.Lerp(0, 90, t);
+            _mIndex.transform.localRotation = Quaternion.Euler(eulers);
+        }
+
+        if (_mMiddle)
+        {
+            Vector3 eulers = GetOriginalEulers(_mMiddle);
+            float t = GetInverseLerpMiddle();
+            eulers.z -= Mathf.Lerp(0, 90, t);
+            _mMiddle.transform.localRotation = Quaternion.Euler(eulers);
+        }
+
+        if (_mRing)
+        {
+            Vector3 eulers = GetOriginalEulers(_mRing);
+            float t = GetInverseLerpRing();
+            eulers.z -= Mathf.Lerp(0, 90, t);
+            _mRing.transform.localRotation = Quaternion.Euler(eulers);
+        }
+
+        if (_mPinky)
+        {
+            Vector3 eulers = GetOriginalEulers(_mPinky);
+            float t = GetInverseLerpPinky();
+            eulers.z -= Mathf.Lerp(0, 90, t);
+            _mPinky.transform.localRotation = Quaternion.Euler(eulers);
+        }
     }
 }
