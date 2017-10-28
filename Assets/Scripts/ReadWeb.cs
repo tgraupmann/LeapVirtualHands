8aaaa28e6b05d9579f931bec706ac37185b7c015
Tim Graupmann <tgraupmann@gmail.com>
2017-10-28 09:35:02 -0700

    Added script to read json data from web

---
+++ Assets/Scripts/ReadWeb.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReadWeb : MonoBehaviour
+{
+
+    public string _mIPAddress = string.Empty;
+    private FingerData _mFingerData = null;
+
+    [System.Serializable]
+    public class FingerData
+    {
+        public int thumb;
+        public int index;
+        public int middle;
+        public int ring;
+        public int pinky;
+    }
+
+    IEnumerator Start()
+    {
+        if (string.IsNullOrEmpty(_mIPAddress))
+        {
+            yield break;
+        }
+        while (true)
+        {
+            string url = string.Format("http://{0}", _mIPAddress);
+            WWW www = new WWW(url);
+            yield return www;
+            string json = www.text;
+            www.Dispose();
+            //Debug.Log(json);
+            _mFingerData = JsonUtility.FromJson<FingerData>(json);
+            yield return null;
+        }
+    }
+
+    private void OnGUI()
+    {
+        GUILayout.BeginVertical(GUILayout.Height(Screen.height));
+        GUILayout.FlexibleSpace();
+
+        if (null != _mFingerData)
+        {
+            GUILayout.BeginHorizontal(GUILayout.Width(Screen.width));
+            GUILayout.Label(string.Format("Thumb: {0}", _mFingerData.thumb));
+            GUILayout.FlexibleSpace();
+            GUILayout.EndHorizontal();
+
+            GUILayout.BeginHorizontal(GUILayout.Width(Screen.width));
+            GUILayout.Label(string.Format("Index: {0}", _mFingerData.index));
+            GUILayout.FlexibleSpace();
+            GUILayout.EndHorizontal();
+
+            GUILayout.BeginHorizontal(GUILayout.Width(Screen.width));
+            GUILayout.Label(string.Format("Middle: {0}", _mFingerData.middle));
+            GUILayout.FlexibleSpace();
+            GUILayout.EndHorizontal();
+
+            GUILayout.BeginHorizontal(GUILayout.Width(Screen.width));
+            GUILayout.Label(string.Format("Ring: {0}", _mFingerData.ring));
+            GUILayout.FlexibleSpace();
+            GUILayout.EndHorizontal();
+
+            GUILayout.BeginHorizontal(GUILayout.Width(Screen.width));
+            GUILayout.Label(string.Format("Pinky: {0}", _mFingerData.pinky));
+            GUILayout.FlexibleSpace();
+            GUILayout.EndHorizontal();
+        }
+
+        GUILayout.FlexibleSpace();
+        GUILayout.EndVertical();
+    }
+}
